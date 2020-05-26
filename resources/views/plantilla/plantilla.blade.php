ac62bb514f53c0307fb2ff52049fffd659ac176f
FREEWJCB <FREEWJCB@users.noreply.github.com>
2020-05-26 14:34:41 -0430

    se agregaron fontawesome y popper

--- resources/views/plantilla/plantilla.blade.php
+++ resources/views/plantilla/plantilla.blade.php
@@ -9,7 +9,8 @@
 
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"  crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}"  crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/all.min.css') }}"  crossorigin="anonymous">
+   
 
     <script src="{{ asset('js/jquery-3.5.1.min.js') }}" crossorigin="anonymous"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}" crossorigin="anonymous" ></script>
