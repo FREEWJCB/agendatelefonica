83359b98a2e1c13ede22e92bdf3d9c16444615cd
FREEWJCB <FREEWJCB@users.noreply.github.com>
2020-05-27 17:52:58 -0430

    se modifico y se creo un archivo navuser

---
+++ resources/views/plantilla/navuser.blade.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-light">
+    <a href="{{ route('agenda.index') }}" class="navbar-brand"><img id="icono" class="img-responsive" src="https://imge.apk.tools/300/d/3/1/com.widesoft.guiatelefonica.png"></a>
+    <ul class="nav flex-column text-center">
+        <li class="nav-item">
+            <span class="nav-link active">Bienvenido Jhonatan</span>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Cerrar sesión</a>
+        </li>
+    </ul>
+
+</nav>
